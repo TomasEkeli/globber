1c0b3853e67321cc78965a6a89ee5c4e4061233d
Tomas Ekeli <tomas@eke.li>
2021-05-27 13:46:29 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace globber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
